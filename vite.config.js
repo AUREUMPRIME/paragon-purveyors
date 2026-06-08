fb05472c3b289acb17919dbf41842eb8a0bd6967
AUREUMPRIME <hgpuogm@gmail.com>
2026-06-07 20:36:20 -0600

    fix: support custom domain base path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 
-const basePath = process.env.VITE_BASE_PATH || "/paragon-purveyors/";
+const basePath = process.env.VITE_BASE_PATH || "/";
 
 export default defineConfig({
   base: basePath,
